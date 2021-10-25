8249bfa1397bd1dd67dccfa12983fe881bee4cd7
Tyler Nevell <tylernevell@gmail.com>
2021-10-25 16:34:19 -0700

    change folder name to pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route } from 'react-router-dom';
-import Products from './components/Products';
-import Welcome from './components/Welcome';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
 
 const App = () => {
   return (
