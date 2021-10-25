ecc0e9f8ba8d1634592d1f9b3ecc3782896d5dd9
Tyler Nevell <tylernevell@gmail.com>
2021-10-25 16:48:10 -0700

    add navlinks and active classes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
 
 const App = () => {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 };
